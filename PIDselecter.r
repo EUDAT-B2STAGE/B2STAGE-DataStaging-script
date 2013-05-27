418570bd86c9b11f5d754584a081ab64cbb5421e
gmariani <gmariani@68e52488-0a15-44bc-a314-416658652264>
2013-05-27 15:18:17 +0000

    First release

---
+++ PIDselecter.r
@@ -0,0 +1,7 @@
+igetPIDrule {
+    getPID(*url, *response);
+    writeLine("stdout","Output: *response");
+}
+
+INPUT *url="irods://data.repo.cineca.it:1247/CINECA01/home/EUDAT_EPOS/EPOSReplica/archive/2008/IV/CAFR/HHE.D/IV.CAFR..HHE.D.2008.239"
+OUTPUT ruleExecOut 
